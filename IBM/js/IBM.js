3601c29b8204892167683acac9041a5ec8c4c326
hyy130202162 <2460218950@qq.com>
2018-01-29 10:18:03 +0800

    lastest

--- IBM/js/IBM.js
+++ IBM/js/IBM.js
@@ -1,4 +1,4 @@
-// 幼斌的弹出框
+// 右边的弹出框
 var tan = document.querySelector(".button-right");
 var close = document.querySelector(".tankai");
 var zz = document.querySelector("#tanzz");
@@ -8,3 +8,10 @@ tan.onclick = function(){
 close.onclick = function(){
 	zz.style.display = "none";
 }
+// 导航栏中点击二级菜单出现相相映的三级菜单
+var secli = document.querySelectorAll(".second-li");
+var menu = document.querySelectorAll(".menu-ri");
+menu[0].style.display = "block";
+menu[13].style.display = "block";
+menu[17].style.display = "block";
+
